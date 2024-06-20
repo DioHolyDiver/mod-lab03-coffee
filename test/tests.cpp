5e6aee2ff83696f6b4a15147c5e184a6791c76ef
DioHolyDiver <163848776+DioHolyDiver@users.noreply.github.com>
2024-06-20 11:52:38 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,64 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+class ATest : public ::testing::Test {
+ protected:
+    Automata vendingMachine;
+
+    void SetUp() override {
+        vendingMachine.on();
+    }
+};
+
+TEST_F(ATest, 1) {
+    Automata newVendingMachine;
+    EXPECT_EQ(newVendingMachine.getState(), STATES::OFF);
+}
+
+TEST_F(ATest, 2) {
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST_F(ATest, 3) {
+    vendingMachine.coin(50);
+    EXPECT_EQ(vendingMachine.getState(), STATES::ACCEPT);
+}
+
+TEST_F(ATest, 4) {
+    EXPECT_NO_THROW(vendingMachine.getMenu());
+}
+
+TEST_F(ATest, 5) {
+    vendingMachine.coin(100);
+    vendingMachine.choice(2);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST_F(ATest, 6) {
+    vendingMachine.coin(10);
+    EXPECT_FALSE(vendingMachine.check(1));
+}
+
+TEST_F(ATest, 7) {
+    vendingMachine.coin(50);
+    vendingMachine.cancel();
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST_F(ATest, 8) {
+    vendingMachine.coin(100);
+    vendingMachine.choice(1);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST_F(ATest, 9) {
+    vendingMachine.coin(100);
+    vendingMachine.choice(1);
+    EXPECT_EQ(vendingMachine.getState(), STATES::WAIT);
+}
+
+TEST_F(ATest, 10) {
+    vendingMachine.choice(1);
+    EXPECT_NE(vendingMachine.getState(), STATES::COOK);
+}
